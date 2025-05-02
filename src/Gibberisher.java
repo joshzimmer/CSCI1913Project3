b1be472338ba0aa33ace33c3882f42e2b0bb4f9c
joshzimmer <112434519+joshzimmer@users.noreply.github.com>
2025-05-01 19:38:59 -0500

    Updated put method to have return

--- src/Gibberisher.java
+++ src/Gibberisher.java
@@ -3,7 +3,25 @@ public class Gibberisher {
     private int segmentLength;
     private int numberOfSegments;
 
-    public Gibberisher() {
-
+    public Gibberisher(int setLength) {
+        segmentLength = setLength;
+        model = new Trie<>();
+        numberOfSegments = 0;
     }
+
+    public void train(String[] words){
+        for(int i = 0; i < words.length; i++) {
+            LetterSample[] samples = LetterSample.toSamples(words[i], segmentLength);
+            for(int j = segmentLength; j <samples.length; j++) {
+                for(int k = 0; k < numberOfSegments; k++) {
+                    model.getNode(samples[j].getSegment().charAt(k));
+                }
+            }
+        }
+    }
+
+    private
+
+
+
 }
--- src/Testing.java
+++ src/Testing.java
@@ -3,11 +3,15 @@ public class Testing {
         return (int)newChar;
     }
     public static void main(String[] args) {
-        System.out.println(letterTest('a'));
-        System.out.println(letterTest('z'));
-        System.out.println(letterTest(Character.toLowerCase('.')));
-        System.out.println(122-97);
-        String letters = "abcdefghijklmnopqrstuvwxyz";
-        System.out.println(letters.charAt(0));
+        String[] words = {"apples", "banana", "grape", "orange", "peel"};
+        for(int i = 0; i<5; i++) {
+            LetterSample[] samples = LetterSample.toSamples(words[i], 3);
+            for(int j = 3; j<samples.length; j++) {
+                System.out.println(samples[j].getSegment());
+                System.out.println(samples[j].getSegment().length());
+                if(j == samples.length - 1)
+                    System.out.println('.');
+            }
+        }
     }
 }
